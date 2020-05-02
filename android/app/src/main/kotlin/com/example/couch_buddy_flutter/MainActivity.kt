41042b4e4a95379b1925e5d79803abd3212fb265
Luca Faggianelli <luca.faggianelli@gmail.com>
2020-05-02 18:14:51 +0200

    remove cast plugin onResume

--- android/app/src/main/kotlin/com/example/couch_buddy_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/couch_buddy_flutter/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.couch_buddy_flutter
 
 import androidx.annotation.NonNull
-import github.showang.flutter_google_cast_button.FlutterGoogleCastButtonPlugin
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
@@ -9,10 +8,5 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
-
-    override fun onResume() {
-        super.onResume()
-        FlutterGoogleCastButtonPlugin.instance?.onResume()
     }
 }
